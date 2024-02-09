9efe75d0dcde01e2b7a4f4f260e13cb30859a5b2
luonggg1124 <luongnm1124@gmail.com>
2024-02-09 21:30:51 +0700

    9/2/2024

---
+++ Section 13/node.js
@@ -0,0 +1,15 @@
+// getAttribute get - thuộc tính
+// scrollIntoView
+//target , classList
+//getBoundingClientRect()
+//contains
+//childNode  firstElementChild lastElementChild parentNode parentElement
+
+//previousElementSibling
+//nextElementSibling
+
+// closest 
+
+// bind
+
+// mouseout mouseover
